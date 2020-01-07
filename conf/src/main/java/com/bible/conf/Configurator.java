0b7101e0161c69905a4accfbdf33097181bd2d19
MowMow <amaury.ollagnier@gmail.com>
2020-01-07 17:33:14 +0100

    Start configuration project

---
+++ conf/src/main/java/com/bible/conf/Configurator.java
@@ -0,0 +1,9 @@
+package com.bible.conf;
+
+public class Configurator{
+
+    public static void main( String[] args){
+        System.out.println("Welcome the bible configurator");
+    }
+    
+}
---
+++ conf/src/main/java/com/bible/conf/IndexConfigurator.java
@@ -0,0 +1,7 @@
+package com.bible.conf;
+
+public class IndexConfigurator{
+
+    
+    
+}
