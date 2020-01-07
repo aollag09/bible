1835d7672db55ede34ea80630a220f0c51b8268e
MowMow <amaury.ollagnier@gmail.com>
2020-01-07 17:49:07 +0100

    Dumy Test

---
+++ conf/src/test/java/com/bible/conf/test/IndexConfiguratorTest.java
@@ -0,0 +1,13 @@
+package com.bible.conf.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IndexConfiguratorTest {
+
+    @Test
+    public void dumy_test(){
+      Assert.assertTrue( "dumy".equals("dumy"));  
+    }
+    
+}
