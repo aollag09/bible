9368a2293ace4fb6ce44814c6e49dd7e09b8f686
Amaury Ollagnier <amaury.ollagnier@gmail.com>
2020-03-14 10:21:50 -0500

    Bing Bang

--- conf/src/main/java/com/bible/conf/Configurator.java
+++
@@ -1,9 +0,0 @@
-package com.bible.conf;
-
-public class Configurator{
-
-    public static void main( String[] args){
-        System.out.println("Welcome the bible configurator");
-    }
-    
-}
--- conf/src/main/java/com/bible/conf/IndexConfigurator.java
+++
@@ -1,7 +0,0 @@
-package com.bible.conf;
-
-public class IndexConfigurator{
-
-    
-    
-}
