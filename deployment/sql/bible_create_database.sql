6c5092aa4afe27b384ee9cf8ad45794b2b737975
Amaury Ollagnier <amaury.ollagnier@gmail.com>
2020-03-14 12:40:35 -0500

    Docker Compose up with MySQL Data

---
+++ deployment/sql/bible_create_database.sql
@@ -0,0 +1,7 @@
+-- Create Production database
+create database bible;
+grant all privileges on database bible to bible_user;
+
+-- Create Test database
+create database bible_test;
+grant all privileges on database bible_test to bible_user;
