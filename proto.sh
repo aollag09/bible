35f32d5f33f063f9e0b686fa7390b1f251a800a4
Amaury Ollagnier <amaury.ollagnier@gmail.com>
2020-04-15 15:06:30 +0200

    Rename back & front by api & web

--- proto.sh
+++ proto.sh
@@ -4,19 +4,19 @@
 PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
 
 # Directory to write generated code to (.js and .d.ts files)
-TS_OUT="./back"
+TS_OUT="./api"
 JS_OUT="./generated/js"
 mkdir -p $TS_OUT
 mkdir -p $JS_OUT
 
 # Look for proto files in back
-PROTO_FILES=$(find ./back -name "*.proto")
+PROTO_FILES=$(find ./api -name "*.proto")
 echo "Generating proto files : "
-echo $PROTO_FILES
+echo "$PROTO_FILES"
 
 protoc \
     --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
     --js_out="import_style=commonjs,binary:${JS_OUT}" \
     --ts_out="${TS_OUT}" \
     --proto_path="./back" \
-    $PROTO_FILES
+    "$PROTO_FILES"
