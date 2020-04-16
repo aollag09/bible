4a0048b0f38aaf2d32792c3a967f020a55cbf025
Amaury Ollagnier <amaury.ollagnier@gmail.com>
2020-04-16 19:41:30 +0200

    App Component

--- web/src/application/index.tsx
+++ web/src/application/index.tsx
@@ -6,7 +6,6 @@ import { Map } from '../map';
 import { People } from '../people';
 import { Read } from '../read';
 import { Search } from '../search';
-import logo from './resources/image/logo.svg';
 import './resources/style/App.css';
 
 export class App extends React.Component {
@@ -14,31 +13,27 @@ export class App extends React.Component {
   render() {
     return (
 
-
-
       <Router>
-        <div className="App">
 
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p> Edit <code>src/App.tsx</code> and save to reload.</p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer">
-              Learn React </a>
+        <div className="bible-site-container">
+
+          <header className="header">
+            <p> Header </p>
           </header>
 
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/read' component={Read} />
-            <Route path='/search' component={Search} />
-            <Route path='/explore' component={Explore} />
-            <Route path='/map' component={Map} />
-            <Route path='/people' component={People} />
+          <div className="content">
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/read' component={Read} />
+              <Route path='/search' component={Search} />
+              <Route path='/explore' component={Explore} />
+              <Route path='/map' component={Map} />
+              <Route path='/people' component={People} />
+            </Switch>
+          </div>
 
-          </Switch>
+          <footer className="footer">
+          </footer>
 
         </div>
       </Router>
--- web/src/home/index.tsx
+++ web/src/home/index.tsx
@@ -5,6 +5,7 @@ export class Home extends React.Component {
   render() {
     return (
       <div className="Home">
+        <p> Bible Home Page .</p>
       </div>
     );
   }
