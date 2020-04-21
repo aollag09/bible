3bf4b44db2da01a9b09855dd3359515fdabeb8fc
MowMow <amaury.ollagnier@gmail.com>
2020-04-21 10:27:34 +0200

    Resize loading gif

--- web/src/common/utils/component/Loading.tsx
+++ web/src/common/utils/component/Loading.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import loadingGif from '../resources/image/loading.gif'
+import '../resources/style/loading.css'
 
 type LoadingProp = {
     text: string
@@ -10,7 +11,9 @@ export class Loading extends Component<LoadingProp>{
     render() {
         return (
             <div className="loading">
-                <img alt="loading gif" src={loadingGif}></img>
+                <div className="loading-image">
+                    <img alt="loading gif" src={loadingGif}></img>
+                </div>
                 <div className="loading-text">
                     <p> {this.props.text}</p>
                 </div>
