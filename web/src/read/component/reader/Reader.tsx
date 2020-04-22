fca707c8dc2f978dba8f2b967bd0a75d66624941
MowMow <amaury.ollagnier@gmail.com>
2020-04-22 09:59:32 +0200

    Previous & Next Button OK

--- web/src/read/component/reader/Reader.tsx
+++ web/src/read/component/reader/Reader.tsx
@@ -14,22 +14,14 @@ type ReaderProp = {
     read: (version: number, book: number, chapter: number) => void
 }
 
-
 type ReaderState = {
-    version: number,
-    book: number,
-    chapter: number,
+
 }
 
 export class Reader extends Component<ReaderProp, ReaderState>{
 
     constructor(props: ReaderProp) {
         super(props)
-        this.state = {
-            version: this.props.version,
-            book: this.props.book,
-            chapter: this.props.chapter
-        }
     }
 
     render() {
@@ -37,9 +29,9 @@ export class Reader extends Component<ReaderProp, ReaderState>{
             return (
                 <div className="reader">
                     <ScriptureReader
-                        version={this.state.version}
-                        book={this.state.book}
-                        chapter={this.state.chapter} />
+                        version={this.props.version}
+                        book={this.props.book}
+                        chapter={this.props.chapter} />
 
                     <div className="reader-button">
                         {this.previousChapterButton()}
@@ -52,32 +44,17 @@ export class Reader extends Component<ReaderProp, ReaderState>{
         }
     }
 
-    componentDidUpdate() {
-        this.state = {
-            version: this.props.version,
-            book: this.props.book,
-            chapter: this.props.chapter
-        }
-    }
-
     nextChapter = () => {
-        let newChapter = this.state.chapter + 1;
-        this.setState({
-            chapter: newChapter
-        })
-        this.props.read(this.state.version, this.state.book, newChapter)
+        this.props.read(this.props.version, this.props.book, this.props.chapter + 1)
     }
 
     previousChapter = () => {
-        let newChapter = this.state.chapter - 1;
-        this.setState({
-            chapter: newChapter
-        })
-        this.props.read(this.state.version, this.state.book, newChapter)
+        let newChapter = this.props.chapter - 1;
+        this.props.read(this.props.version, this.props.book, this.props.chapter - 1)
     }
 
     previousChapterButton() {
-        let chapter = this.state.chapter;
+        let chapter = this.props.chapter;
         if (chapter > 1) {
             return (
                 <button
@@ -92,7 +69,7 @@ export class Reader extends Component<ReaderProp, ReaderState>{
     }
 
     nextChapterButton = () => {
-        let chapter = this.state.chapter;
+        let chapter = this.props.chapter;
         let nbChapters = 20;// parseInt(useFetch(BibleAPI.url + "book/" + this.props.book + "/chapters/count").toString());
         if (chapter < nbChapters) {
             return (
