f7a36e61b4f909f065c489ef85bfb25b59d71a8d
Amaury Ollagnier <amaury.ollagnier@gmail.com>
2020-04-29 20:20:47 +0200

    Tag Factory Form

--- web/src/read/component/tagger/TagFactory.tsx
+++ web/src/read/component/tagger/TagFactory.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button } from '@material-ui/core';
 
 
@@ -12,13 +12,32 @@ type TagFactoryProps = {
 
 export const TagFactory: React.FunctionComponent<TagFactoryProps> = props => {
 
+    const [type, setType] = useState<string>();
+    const [subtype, setSubtype] = useState<string>();
+
+    const [what, setWhat] = useState<string>();
+    const [whatDetails, setWhatDetails] = useState<string>();
+
+    const [who, setWho] = useState<string>();
+
+    const [where, setWhere] = useState<string>();
+    const [latitude, setLatitude] = useState<number>();
+    const [longitude, setLongitude] = useState<number>();
+
+    const [year, setYear] = useState<number>();
+
+    const [how, setHow] = useState<string>();
+    const [howDetails, setHowDetails] = useState<string>()
+
     const renderTagType = (tagType: string) => {
         switch (tagType) {
             case 'what':
                 return (
                     <div className="tag-factory-what">
-                        <TextField required id="what" label="What" variant="filled" defaultValue="" />
-                        <TextField multiline id="what-details" label="Details" variant="filled" defaultValue="" />
+                        <TextField required id="what" label="What" variant="filled" defaultValue=""
+                            onChange={(event => { setWhat(event.target.value) })} />
+                        <TextField multiline id="what-details" label="Details" variant="filled" defaultValue=""
+                            onChange={(event => { setWhatDetails(event.target.value) })} />
 
                     </div>
                 );
@@ -26,31 +45,38 @@ export const TagFactory: React.FunctionComponent<TagFactoryProps> = props => {
             case 'who':
                 return (
                     <div className="tag-factory-who">
-                        <TextField required id="who" label="Who" variant="filled" defaultValue="" />
+                        <TextField required id="who" label="Who" variant="filled" defaultValue=""
+                            onChange={(event => { setWho(event.target.value) })} />
                     </div>
                 );
 
             case 'where':
                 return (
                     <div className="tag-factory-where">
-                        <TextField required id="where" label="What" variant="filled" defaultValue="" />
-                        <TextField id="latitude" type="number" variant="filled" label="Latitude" defaultValue="" />
-                        <TextField id="longitude" type="number" variant="filled" label="Longitude" defaultValue="" />
+                        <TextField required id="where" label="Where" variant="filled" defaultValue=""
+                            onChange={(event => { setWhere(event.target.value) })} />
+                        <TextField id="latitude" type="number" variant="filled" label="Latitude" defaultValue=""
+                            onChange={(event => { setLatitude(parseInt(event.target.value)) })} />
+                        <TextField id="longitude" type="number" variant="filled" label="Longitude" defaultValue=""
+                            onChange={(event => { setLongitude(parseInt(event.target.value)) })} />
                     </div>
                 );
 
             case 'when':
                 return (
                     <div className="tag-factory-when">
-                        <TextField id="year" type="number" variant="filled" label="Year" defaultValue="" />
+                        <TextField id="year" type="number" variant="filled" label="Year" defaultValue=""
+                            onChange={(event => { setYear(parseInt(event.target.value)) })} />
                     </div>
                 );
 
             case 'how':
                 return (
                     <div className="tag-factory-how">
-                        <TextField required id="how" label="How" variant="filled" defaultValue="" />
-                        <TextField multiline id="how-details" label="Details" variant="filled" defaultValue="" />
+                        <TextField required id="how" label="How" variant="filled" defaultValue=""
+                            onChange={(event => { setHow(event.target.value) })} />
+                        <TextField multiline id="how-details" label="Details" variant="filled" defaultValue=""
+                            onChange={(event => { setHowDetails(event.target.value) })} />
 
                     </div>
                 );
@@ -60,20 +86,27 @@ export const TagFactory: React.FunctionComponent<TagFactoryProps> = props => {
         }
     }
 
+    /** Create the tag */
+    const create = () => {
+
+    }
+
     if (props.tagType) {
 
         return (
             <div className="tag-factory">
                 <div className="tag-factory-form">
 
-
-                    <TextField id="type" label="Type" variant="filled" defaultValue="" />
-                    <TextField id="subtype" label="Sub Type" variant="filled" defaultValue="" />
+                    <TextField id="type" label="Type" variant="filled" defaultValue=""
+                        onChange={(event => { setType(event.target.value) })} />
+                    <TextField id="subtype" label="Sub Type" variant="filled" defaultValue=""
+                        onChange={(event => { setSubtype(event.target.value) })} />
 
                     {renderTagType(props.tagType)}
 
-
-                    <Button > Create </Button>
+                    <Button type="submit" className="button-submit" >
+                        Create
+                    </Button>
                 </div>
 
             </div>
