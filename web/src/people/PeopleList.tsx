d35156cad418c912559d6b7646800969137f34de
Amaury Ollagnier <amaury.ollagnier@gmail.com>
2020-05-04 19:34:48 +0200

    Display People list

---
+++ web/src/people/PeopleList.tsx
@@ -0,0 +1,53 @@
+
+import { List, ListItem, ListItemText } from "@material-ui/core";
+import Axios from "axios";
+import { Message } from "google-protobuf";
+import React, { useEffect, useState } from "react";
+import { Peoples } from "../common/generated/services/people/people_pb";
+import { BibleAPI } from "../common/utils/bibleAPI";
+
+type PeopleListProp = {
+    handlePeopleSelect: (peopleid: number) => void,
+}
+
+/**
+ * Simple list selector for mobile
+ * @param props 
+ */
+export const PeopleList: React.FunctionComponent<PeopleListProp> = (props) => {
+
+    const [peoples, setPeoples] = useState<Peoples>();
+
+    useEffect(() => {
+        if (!peoples) {
+            fetchPeoples()
+        }
+    }, [peoples]);
+
+
+    const fetchPeoples = async () => {
+        const res = await Axios.get<string>(BibleAPI.url + "people/language/1")
+        setPeoples(Peoples.deserializeBinary(Message.bytesAsU8(res.data)))
+    }
+
+    const peopleList: JSX.Element[] = []
+
+    if (peoples) {
+        peoples.getPeoplesList().forEach(people => {
+            peopleList.push(
+                <ListItem button key={"list-item-people" + people.getId()} onClick={() => props.handlePeopleSelect(people.getId())}>
+                    <ListItemText key={"list-item-text-people-" + people.getId()} primary={people.getId() + " - " + people.getName() + " - " + people.getDescription()} />
+                </ListItem>
+            )
+        })
+    }
+
+    return (
+
+        <div className="people-list-selector">
+            <List>
+                {peopleList}
+            </List>
+
+        </div>);
+}
--- web/src/people/index.tsx
+++ web/src/people/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar } from '../navbar';
+import { PeopleList } from './PeopleList';
 
 export class People extends React.Component {
 
@@ -7,6 +8,7 @@ export class People extends React.Component {
     return (
       <div className="bible-content people">
             <Navbar />
+            <PeopleList handlePeopleSelect={(id) => console.log(id)} />
       </div>
     );
   }
